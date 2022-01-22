505a907c87f3dc33a1917c1d8285508f1cf75230
Tyler Smith <Tysocialhq@gmail.com>
2022-01-21 21:35:58 -0600

    NavBar Commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './navbar.css';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo2.svg'
+
+//BEM - Block Element Modifier
+
+const Menu = () => (
+
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#what">About</a></p>
+  <p><a href="#ai">AI</a></p>
+  <p><a href="#blog">Blog</a></p>
+  <p><a href="#lib">Library</a></p>
+  </>
+)
+
+const Navbar = () => {
+  const[toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className='thoth__navbar'>
+      <div className="thoth__navbar-links">
+        <div className="thoth__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+<div className="thoth__navbar-links_container">
+  <Menu />
+</div>
+      </div>
+      <div className="thoth__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+      <div className="thot__navbar-menu">
+        {toggleMenu
+        ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
+        : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>
+      }
+      {toggleMenu && (
+        <div className='thoth__navbar-menu_container scale-up-center'>
+          <div className='thoth__navbar-menu_container-links'>
+            <Menu />
+            <div className="thoth__navbar-menu_container-links-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+          </div>
+        </div>
+      )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/containers/whatthoth/WhatThoth.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatthoth.css';
+
+const WhatThoth = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WhatThoth
