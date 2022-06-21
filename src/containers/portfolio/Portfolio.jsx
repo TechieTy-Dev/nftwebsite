fae3c800c3b06301073bc6d39e0bd7ee2ad039bc
Tyler Smith <Tysocialhq@gmail.com>
2022-06-21 15:00:18 -0500

    Added utopia to portfolio

--- src/containers/portfolio/Portfolio.jsx
+++ src/containers/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Article } from '../../components';
 import harpocrypto from '../../assets/harpocrypto.PNG'
 import realestate from '../../assets/real-estate.PNG'
-import punks from '../../assets/punks-clone.png';
+import utopia from '../../assets/utopia.PNG'
 import thoth from '../../assets/thoth.png';
 import './portfolio.css';
 
@@ -18,8 +18,8 @@ const Portfolio = () => {
         <div className="thoth__portfolio_groupB">
           <Article imgUrl={harpocrypto}  url="https://harpocrypto.vercel.app/" text="Crypto Dashboard built in React, AntDesign, CoinMarketCo API to get real coin data, Bing API for real time blog posts on crypto and deployed and hosted on Vercel " date="June 6, 2022"  title="HarpoCrypto" />
           <Article imgUrl={realestate} text="Real Estate Website built in NextJS with Real Estate API and styled with Chakra-UI."date="June 1, 2022" title="Real Estate Group" url="https://real-estate-group.vercel.app/"/>
-          <Article imgUrl={punks} text="OpenSea Clone made in React and hosted on Firebase"date="Sept 16, 2021" title="Punks Clone"/>
-          <Article imgUrl={thoth} text="OpenSea Clone made in React and hosted on Firebase"date="Sept 16, 2021" title="Punks Clone" url="https://thothio.firebaseapp.com/" />
+          <Article imgUrl={utopia} url="https://crypto-utopia.vercel.app" text="Crypto Website deployed and hosted on Vercel"date="June 21, 2022" title="Utopia"/>
+          <Article imgUrl={thoth} text="Thoth made in React and hosted on Firebase"date="Sept 16, 2021" title="Punks Clone" url="https://thothio.firebaseapp.com/" />
         </div>
       </div>
       
