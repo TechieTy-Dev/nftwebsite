35d66bbe8cd4e326a59857e58a70b8e60bef6d78
Tyler Smith <Tysocialhq@gmail.com>
2022-06-22 15:57:59 -0500

    Iris Added

--- src/containers/portfolio/Portfolio.jsx
+++ src/containers/portfolio/Portfolio.jsx
@@ -4,6 +4,7 @@ import harpocrypto from '../../assets/harpocrypto.PNG'
 import realestate from '../../assets/real-estate.PNG'
 import utopia from '../../assets/utopia.PNG'
 import thoth from '../../assets/thoth.png';
+import iris from '../../assets/iris.PNG';
 import './portfolio.css';
 
 const Portfolio = () => {
@@ -21,9 +22,11 @@ const Portfolio = () => {
           <a href="https://real-estate-group.vercel.app" target="_blank" rel="noreferrer">
           <Article imgUrl={realestate} text="Real Estate Website built in NextJS with Real Estate API and styled with Chakra-UI."date="June 1, 2022" title="Real Estate Group" url="https://real-estate-group.vercel.app/"/></a>
           <a href="https://crypto-utopia.vercel.app" target="_blank" rel="noreferrer">
-          <Article imgUrl={utopia} url="https://crypto-utopia.vercel.app" text="Crypto Website deployed and hosted on Vercel"date="June 21, 2022" title="Utopia"/></a>
+          <Article imgUrl={utopia} url="https://crypto-utopia.vercel.app" text="Crypto Website - FrontEnd is React/Tailwind - Backend is deployed and hosted on Vercel"date="June 21, 2022" title="Utopia"/></a>
           <a href="https://thothio.firebaseapp.com" target="_blank" rel="noreferrer">
           <Article imgUrl={thoth} text="Thoth made in React and hosted on Firebase"date="Sept 16, 2021" title="Punks Clone" url="https://thothio.firebaseapp.com/" /></a>
+          <a href="https://iris-bc.vercel.app/" target="_blank" rel="noreferrer">
+          <Article imgUrl={iris} text="Iris is a twitter clone. Frontend is built on React, backend uses Sanity for a database that stores tweets and users, and Vercel for deployment. MetaMask is needed to use Demo."date="June 22, 2022" title="Iris - Social App" url="https://iris-bc.vercel.app/" /></a>
         </div>
       </div>
       
